9b8aa5906385281afcf57cfa9b8fb8224b07bda1
Leo GIMENEZ-FRONTIN <lgimenez@bess-f3r4s4.clusters.42paris.fr>
2024-12-18 12:40:47 +0100

    fix: improved main

--- srcs/main.c
+++ srcs/main.c
@@ -1,53 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgimenez <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/18 11:45:30 by lgimenez          #+#    #+#             */
+/*   Updated: 2024/12/18 12:38:26 by lgimenez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 
-#define	STR1	"Hello"
-#define	STR2	"How are you"
-#define	STR3	"Fine!"
+#define STR1 "Hello"
+#define STR2 "How are you"
+#define STR3 "Fine!"
+
+size_t	ft_strlen(const char *s);
+char	*ft_strcpy(char *dest, char *src);
+int		ft_strcmp(const char *s1, const char *s2);
+ssize_t	ft_write(int fd, const void *buf, size_t count);
+ssize_t	ft_read(int fd, const void *buf, size_t count);
+char	*ft_strdup(const char *s);
 
 
-void	ft_testStrlen()
+void	ft_test_strlen(void)
 {
 	printf("strlen: %ld\n", strlen(STR1));
-	printf("ft_strlen: %ld\n", ft_strlen(STR1));
+	printf("ft_strlen: %d\n", ft_strlen("bonjour"));
 }
 
-void	ft_testStrcpy()
+void	ft_test_strcpy(void)
 {
 
 }
 
-void	ft_testStrcmp()
+void	ft_test_strcmp(void)
+{
+//	printf("ft_strcmp: %d\n", ft_strcmp(STR1, STR2));
+}
+
+void	ft_test_write(void)
 {
 
 }
 
-void	ft_testWrite()
-{
-
-}
-
-void	ft_testRead()
+void	ft_test_read(void)
 {
 
 }
 
 
-void	ft_testStrdup()
+void	ft_test_strdup(void)
 {
 
 }
 
 int	main(void)
 {
-	ft_testStrlen();
-	ft_testStrcpy();
-	ft_testStrcmp();
-	ft_testWrite();
-	ft_testRead();
-	ft_testStrdup();
-	return 0;
+	ft_test_strlen();
+	ft_test_strcpy();
+	ft_test_strcmp();
+	ft_test_write();
+	ft_test_read();
+	ft_test_strdup();
+
+	return (0);
 }
